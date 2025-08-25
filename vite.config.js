6b76247b325c5d61737b968bdaf155b4276d5713
MrTanapat <tan.tanapat2002@gmail.com>
2025-08-25 17:58:59 +0700

    feat: Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-// vite.config.js - ปรับแต่งสำหรับ deployment
+// // vite.config.js - ปรับแต่งสำหรับ deployment
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/my-portfolio-website",
+//   build: {
+//     outDir: "dist",
+//     assetsDir: "assets",
+//     sourcemap: false,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio-website",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-  },
+  base: "/my-portfolio-website", // <-- เพิ่มหรือแก้ไขบรรทัดนี้
 });
